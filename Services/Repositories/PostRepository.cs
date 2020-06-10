f5d08f1c0691b2f2a533bfadc41b1074d20ff1ff
eldom90 <66010734+eldom90@users.noreply.github.com>
2020-06-10 20:16:40 +0200

    Add files via upload

---
+++ Services/Repositories/PostRepository.cs
@@ -0,0 +1,79 @@
+﻿using BlogPostApp.API.Services.Helpers;
+using BlogPostApp.Services.Interfaces;
+using BlogPostApp.Services.Mappers;
+using BlogPostApp.Services.ViewModel.Post;
+using BlogPostData.Context;
+using BlogPostData.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogPostApp.Services.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly ApplicationDbContext _blogpostContext;
+        private ITagRepository _tagRepositroy;
+
+        public PostRepository(ApplicationDbContext blogpostContext, ITagRepository tagRepositroy)
+        {
+            _blogpostContext = blogpostContext;
+            _tagRepositroy = tagRepositroy;
+        }
+
+        public void Create(PostViewModel entity)
+        {
+            PostMapper _postMapper = new PostMapper();
+
+            Post post = _postMapper.Map(entity);
+            _blogpostContext.Post.Add(post);
+            _blogpostContext.SaveChanges();
+           
+        }
+
+        public void Delete(string slug)
+        {
+            var posts = _blogpostContext.Post.Where(p => p.slug == slug).FirstOrDefault();
+
+            List<Tag> tag = _blogpostContext.Tag.Where(t => t.postId == posts.slug).ToList();
+            foreach (var item in tag)
+            {
+                _blogpostContext.Tag.Remove(item);
+                _blogpostContext.SaveChanges();
+            }
+
+            _blogpostContext.Post.Remove(posts);
+            _blogpostContext.SaveChanges();
+        }
+
+        public List<Post> Get()
+        {
+            return _blogpostContext.Post.ToList();
+        }
+
+        public void Update(PostViewModel entity, string slugId)
+        {
+            List<Tag> tag = _blogpostContext.Tag.Where(t => t.postId == slugId).ToList();
+            foreach (var item in tag)
+            {
+                Tag tagTemp = item;
+                tagTemp.postId = entity.slug;
+                _tagRepositroy.Update(tagTemp);
+            }
+
+            Post postentity = _blogpostContext.Post.Where(p => p.slug == slugId).FirstOrDefault();
+            if (entity.title != null)
+            {
+                postentity.slug = entity.slug;
+                postentity.title = entity.title;
+            }
+            postentity.description = entity.description;
+            postentity.body = entity.body;
+            postentity.updatedAt = DateTime.Now;
+
+            _blogpostContext.Entry(postentity).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _blogpostContext.SaveChanges();
+        }
+    }
+}
---
+++ Services/Repositories/TagRepositroy.cs
@@ -0,0 +1,44 @@
+﻿using BlogPostApp.Services.Interfaces;
+using BlogPostApp.Services.Mappers;
+using BlogPostApp.Services.ViewModel.Tag;
+using BlogPostData.Context;
+using BlogPostData.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogPostApp.Services.Repositories
+{
+    public class TagRepositroy : ITagRepository
+    {
+        private readonly ApplicationDbContext _blogpostContext;
+
+        public TagRepositroy(ApplicationDbContext blogpostContext)
+        {
+            _blogpostContext = blogpostContext;
+        
+        }
+
+        public void Create(TagViewModel entity)
+        {
+            TagMapper _tagMapper = new TagMapper();
+
+            Tag tag = _tagMapper.Map(entity);
+            _blogpostContext.Tag.Add(tag);
+            _blogpostContext.SaveChanges();      
+        }
+
+        public List<Tag> Get()
+        {
+            return _blogpostContext.Tag.AsNoTracking().ToList();
+        }
+
+        public void Update(Tag entity)
+        {
+            _blogpostContext.Entry(entity).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _blogpostContext.SaveChanges();
+        }
+    }
+}
