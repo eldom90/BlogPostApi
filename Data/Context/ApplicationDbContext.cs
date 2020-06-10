c914724d2707a00f309f4070beab1d11ba9d8c73
eldom90 <66010734+eldom90@users.noreply.github.com>
2020-06-10 20:24:23 +0200

    Add files via upload

---
+++ Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Web;
+using BlogPostData.Model;
+
+namespace BlogPostData.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Post> Post { get; set; }
+        public DbSet<Tag> Tag { get; set; }
+       
+    }
+}
