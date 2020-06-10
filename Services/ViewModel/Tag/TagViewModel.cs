416a4dff650a9b2f32b6435263d255061c1140d5
eldom90 <66010734+eldom90@users.noreply.github.com>
2020-06-10 20:19:09 +0200

    Add files via upload

---
+++ Services/ViewModel/Tag/TagViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogPostApp.Services.ViewModel.Tag
+{
+    public class TagViewModel
+    {
+        public string tagName { get; set; }
+        public string postId { get; set; }
+    }
+}
