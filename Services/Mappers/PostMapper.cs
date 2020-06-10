a0bda415f7722cdb5407ebb26f74fadb39a90acb
eldom90 <66010734+eldom90@users.noreply.github.com>
2020-06-10 20:16:04 +0200

    Add files via upload

---
+++ Services/Mappers/PostMapper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogPostApp.Services.ViewModel.Post;
+using BlogPostData.Model;
+using Microsoft.IdentityModel.Tokens;
+
+
+namespace BlogPostApp.Services.Mappers
+{
+    public class PostMapper
+    {   
+        public PostMapper()
+        {
+          
+        }
+
+        public PostViewModel Map(Post post)
+        {
+            return new PostViewModel()
+            {
+                slug = post.slug,
+                title = post.title, 
+                description = post.description,
+                body = post.body,
+                createdAt = post.createdAt,
+                updatedAt = post.updatedAt
+            };
+        }
+
+        public Post Map(PostViewModel post)
+        {
+            return new Post()
+            {
+                slug = post.slug,
+                title = post.title,
+                description = post.description,
+                body = post.body,
+                createdAt = post.createdAt,
+                updatedAt = post.updatedAt
+            };
+        }
+    }
+}
---
+++ Services/Mappers/TagMapper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogPostApp.Services.ViewModel.Tag;
+using BlogPostData.Model;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BlogPostApp.Services.Mappers
+{
+    public class TagMapper
+    {
+        public TagMapper()
+        {
+
+        }
+
+        public TagViewModel Map(Tag tag)
+        {
+            return new TagViewModel()
+            {
+               
+                postId= tag.postId,
+                tagName = tag.tagName
+            };
+        }
+
+        public Tag Map(TagViewModel tag)
+        {
+            return new Tag()
+            {
+                postId= tag.postId,
+                tagName = tag.tagName
+            };
+        }
+    }
+}
