77d8171cb9795dd4b94b7ca74ef486f66f37a515
eldom90 <66010734+eldom90@users.noreply.github.com>
2020-06-10 20:24:54 +0200

    Add files via upload

---
+++ Data/Model/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BlogPostData.Model
+{
+    public class Post
+    {
+        [Key]
+        public int postId { get; set; }
+        public string slug { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string body { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+    }
+}
---
+++ Data/Model/Tag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BlogPostData.Model
+{
+    public class Tag
+    {
+        [Key]
+        public int tagId { get; set; }
+        public string tagName { get; set; }
+        public string postId { get; set; }
+
+    }
+}
