5a8e653197a961a75cf37ee94d77f73af987b78e
eldom90 <66010734+eldom90@users.noreply.github.com>
2020-06-10 20:15:32 +0200

    Add files via upload

---
+++ Services/Interfaces/IPostRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogPostApp.Services.ViewModel.Post;
+using BlogPostData.Model;
+
+namespace BlogPostApp.Services.Interfaces
+{
+    public interface IPostRepository
+    {
+        List<Post> Get();
+        void Create(PostViewModel entity);
+        void Delete(string slug);
+        void Update(PostViewModel entity,string slugId);
+    }
+}
---
+++ Services/Interfaces/ITagRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogPostApp.Services.ViewModel.Tag;
+using BlogPostData.Model;
+
+namespace BlogPostApp.Services.Interfaces
+{
+    public interface ITagRepository
+    {
+        List<Tag> Get();
+        void Create(TagViewModel entity);
+        void Update(Tag entity);
+    }
+}
