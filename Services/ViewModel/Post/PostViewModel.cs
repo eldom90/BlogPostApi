846951dfe7cae4aba4bf82fd7dcaa11f18ba9f72
eldom90 <66010734+eldom90@users.noreply.github.com>
2020-06-10 20:17:19 +0200

    Add files via upload

---
+++ Services/ViewModel/Post/PostViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogPostApp.Services.ViewModel.Post
+{
+    public class PostViewModel
+    {
+        public string slug { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string body { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+    }
+}
